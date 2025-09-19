2744c6b638c39d1de655d4f29e0a29e45592dbcb
lucas <lucas1932003@gmail.com>
2025-09-19 08:30:15 -0300

    CAMBIOS JETKINS

--- TestExam/Entity/MateriaPrima.cs
+++ TestExam/Entity/MateriaPrima.cs
@@ -4,5 +4,5 @@ public class MateriaPrima
 {
     public int Id { get; set; }
     public string Nombre { get; set; }
-    public string Descripcion { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty; 
 }
